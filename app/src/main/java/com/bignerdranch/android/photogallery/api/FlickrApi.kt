00ed81ff747e5717b8d99e6ffaa183e7ac8dba1a
jchu62 <89236068+jchu62@users.noreply.github.com>
2023-10-24 22:03:40 -0700

    Successfully obscure API key

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.photogallery.api
 
+import com.bignerdranch.android.photogallery.BuildConfig
 import retrofit2.http.GET
+
+private const val API_KEY = BuildConfig.API_KEY;
 
 interface FlickrApi {
     @GET("/")
